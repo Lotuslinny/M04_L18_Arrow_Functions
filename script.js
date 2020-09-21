23e9fd09af2c982992625caba5be8133acb18ff3
Lotuslinny <linny@fegg.nl>
2020-09-21 08:57:14 +0200

    Basic arrow functions

---
+++ script.js
@@ -0,0 +1,32 @@
+//Deel A
+// zet de functie om naar een arrow functie
+/* const ikRockArrowFunctions = function () {
+  console.log("Joe, ik rock de arrow functions!");
+};
+ */
+ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+ikRockArrowFunctions();
+
+//Deel B
+/* const fivePlusSeven = function () {
+  return 5 + 7
+};
+fivePlusSeven(); */
+
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+
+//Deel C Anonymous function
+var z = 1 + 2
+
+//Deel D
+let sum2 = (a, b) => a + b
+
+//Deel E
+let AddFive = a => a + 5
+
+//console.log(AddFive(5)); De terminal geeft dan 10.
+
+//Deel F
+const createObject = (greeting) => console.log(createObject("hoi"));
